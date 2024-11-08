7bce4aff52bfbdd7248f2d97930edf18c2bfcfd0
Maton666 <134968623+Maton666@users.noreply.github.com>
2024-11-08 00:36:44 -0300

    Entrega2casifinal

--- src/app/mantenedor/mantenedor.page.ts
+++ src/app/mantenedor/mantenedor.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { APIControllerService } from 'src/app/servicios/apicontroller.service';
+import { StorageService } from '../servicios/storage.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mantenedor',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MantenedorPage implements OnInit {
 
-  constructor() { }
+  usuarios: any[] = [];
+
+  constructor(
+    private api: APIControllerService,
+    private storageService: StorageService,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+    this.cargarUsuarios();
   }
 
+  cargarUsuarios() {
+    this.api.getUsuarios().subscribe(
+      (data) => {
+        this.usuarios = data;
+        console.log(this.usuarios);
+      },
+      (error) => {
+        console.error("Error al cargar los usuarios: " + error);
+      }
+    );
+  }
+
+  modificarUsuario(id: string) {
+    this.navCtrl.navigateForward(`/moduser/${id}`);
+  }
+
+  async eliminarUsuario(id: string) {
+    try {
+      await this.api.deleteUsuarios(id).toPromise();
+      console.log(`Usuario con ID: ${id} eliminado correctamente.`);
+      await this.storageService.remove(id);
+      console.log(`Usuario con ID: ${id} eliminado del almacenamiento local correctamente.`);
+      this.cargarUsuarios(); // Recargar los usuarios después de eliminar
+    } catch (error) {
+      console.error('Error al eliminar el usuario:', error);
+    }
+  }
+
+  irARegistro() {
+    this.navCtrl.navigateForward('/registro');
+  }
 }
