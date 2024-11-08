ee7860afe98de24c8fe0bbde22c08c650a68eb78
Maton666 <134968623+Maton666@users.noreply.github.com>
2024-11-08 01:02:17 -0300

    EntregaFINALDEFINITIVAv2

--- src/app/servicios/authenticator.service.ts
+++ src/app/servicios/authenticator.service.ts
@@ -14,22 +14,22 @@ export class AuthenticatorService {
 
   async loginBDD(email: string, password: string): Promise<boolean> {
     try {
-      // Llamamos a la API para obtener todos los usuarios
+
       const usuarios = await this.apiService.getUsuarios().toPromise();
 
-      // Buscamos el usuario que coincida con el correo y la contraseña
+
       const usuarioEncontrado = usuarios.find(
         (usuario: any) => usuario.email === email && usuario.password === password
       );
 
       if (usuarioEncontrado) {
-        // Si el usuario se encuentra, actualizamos el estado de conexión y guardamos en el almacenamiento local
+       
         this.connectionStatus = true;
         await this.storage.set('currentUser', usuarioEncontrado);  // Guardamos el usuario actual en el almacenamiento
         console.log('Login exitoso:', usuarioEncontrado);
         return true;
       } else {
-        // Si el usuario no se encuentra, establecemos el estado de conexión como falso
+        // Si el usuario no se encuentra establecemos el estado de conexion como falso
         this.connectionStatus = false;
         return false;
       }
@@ -47,12 +47,12 @@ export class AuthenticatorService {
 
   logout() {
     this.connectionStatus = false;
-    this.storage.remove('currentUser'); // Eliminamos el usuario actual del almacenamiento al cerrar sesión
+    this.storage.remove('currentUser'); 
   }
 
   async registrar(usuario: any): Promise<boolean> {
     try {
-      // Intentamos registrar el usuario en la base de datos a través del APIControllerService
+      
       const res = await this.apiService.postUsuarios(usuario).toPromise();
       if (res) {
         console.log('Usuario registrado en la base de datos:', usuario);
